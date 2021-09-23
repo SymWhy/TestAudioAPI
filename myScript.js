8ec84c8966be64d1643e1e5d7c648d9ce13695b2
SymWhy <simone.whysel@gmail.com>
2021-09-23 19:57:13 -0400

    first commit

---
+++ myScript.js
@@ -0,0 +1,37 @@
+// JavaScript source code
+
+	// Create an AudioContext
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioCtx = new AudioContext();
+
+    //identify each HTML element you want to use
+    const audioElement = document.querySelector('audio');
+    const playBtn = document.querySelector('button');
+    const volumeSlider = document.querySelector('.volume');
+
+    const audioSource = audioCtx.createMediaElementSource(audioElement);
+
+    //if the button is clicked, pause or play
+    playBtn.addEventListener('click', function() {
+        if (audioCtx.state === 'suspended') {
+            audioCtx.resume();
+        }
+        //if the audio is paused, play it
+        if (this.getAttribute('class') === 'paused') {
+        audioElement.play();
+        this.setAttribute('class', 'playing');
+        this.textContent = 'Pause';
+        }
+        //if the audio is playing, pause it
+        else if (this.getAttribute('class') === 'playing') {
+            audioElement.pause();
+            this.setAttribute('class', 'paused');
+            this.textContent = 'Play';
+        }
+    });
+
+    //reset audio to beginning
+    audioElement.addEventListener('ended', function() {
+        playBtn.setAttribute('class', 'paused');
+        playBtn.textContent = 'Play';
+    });
